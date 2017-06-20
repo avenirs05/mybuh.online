7a0dc465720f8377126d69c33a939abaadd66abf
avenirs05 <avenirs05@gmail.com>
2017-06-20 19:44:17 +0300

    begin

---
+++ events-general.js
@@ -0,0 +1,23 @@
+$(function () {
+
+	$('#btn-free-consult').click(function() {
+		$('#modal-free-consult').modal('show');
+	});
+
+	$('#btn-free-consult-mob').click(function() {
+		$('#modal-free-consult-mob').modal('show');
+	});
+
+	$('#a-free-consult-footer').click(function() {
+		$('#modal-free-consult').modal('show');
+	});
+
+	$('#btn-get-bonus').click(function() {
+		$('#modal-for-accountants').modal('show');
+	});
+
+	$('#btn-get-bonus-mob').click(function() {
+		$('#modal-for-accountants-mob').modal('show');
+	});
+
+});
---
+++ header.php
@@ -0,0 +1,130 @@
+<div class="modal fade" id="modal-free-consult" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Заполните данные ниже и мы вас проконсультируем по этому вопросу.</h4>
+            </div>
+            <div class="modal-body">
+                <form action="handler-free.php" method="get">
+                    <div><div>Имя</div><input id="name-envd" type="text" name="name-free-consult"></div>
+                    <div><div>Телефон</div><input id="phone-envd" type="text" name="phone-free-consult"></div>
+                    <div><div>Email</div><input id="email-envd" type="text" name="email-free-consult"></div>
+                    <input type="submit" value="Отправить" name="submit-free-consult">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-free-consult-mob" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Заполните данные ниже и мы вас проконсультируем по этому вопросу.</h4>
+            </div>
+            <div class="modal-body">
+                <form action="handler-free.php" method="get">
+                    <div><div>Имя</div><input id="name-envd" type="text" name="name-free-consult"></div>
+                    <div><div>Телефон</div><input id="phone-envd" type="text" name="phone-free-consult"></div>
+                    <div><div>Email</div><input id="email-envd" type="text" name="email-free-consult"></div>
+                    <input type="submit" value="Отправить" name="submit-free-consult">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-free-consult-footer" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Заполните данные ниже и мы вас проконсультируем по этому вопросу.</h4>
+            </div>
+            <div class="modal-body">
+                <form action="handler-free.php" method="get">
+                    <div><div>Имя</div><input id="name-envd" type="text" name="name-free-consult"></div>
+                    <div><div>Телефон</div><input id="phone-envd" type="text" name="phone-free-consult"></div>
+                    <div><div>Email</div><input id="email-envd" type="text" name="email-free-consult"></div>
+                    <input type="submit" value="Отправить" name="submit-free-consult">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-for-accountants" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Для получения бонуса заполните данные ниже</h4>
+            </div>
+            <div class="modal-body">
+                <form action="handler-free.php" method="get">
+                    <div><div>Названия Ваших фирм</div><input id="names-of-companies" type="text" name="names-of-companies"></div>
+                    <div><div>Имя</div><input id="name-of-buh" type="text" name="name-of-buh"></div>
+                    <div><div>Телефон</div><input id="phone-of-buh" type="text" name="phone-of-buh"></div>
+                    <input type="submit" value="Отправить" name="submit-get-bonus">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="modal-for-accountants-mob" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Для получения бонуса заполните данные ниже</h4>
+            </div>
+            <div class="modal-body">
+                <form action="handler-free.php" method="get">
+                    <div><div>Названия Ваших фирм</div><input id="names-of-companies" type="text" name="names-of-companies"></div>
+                    <div><div>Имя</div><input id="name-of-buh" type="text" name="name-of-buh"></div>
+                    <div><div>Телефон</div><input id="phone-of-buh" type="text" name="phone-of-buh"></div>
+                    <input type="submit" value="Отправить" name="submit-get-bonus">
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="main-screen visible-md-block visible-lg-block">
+    <div class="header visible-md-block visible-lg-block">
+        <div class="row">
+            <div class="col-md-12">
+                <a href="index.php"><img src="imgs/logo.png" alt=""></a>
+                <div class="right-side">
+                    <button id="btn-free-consult">Бесплатная консультация</button>
+                    <div class="cons-phone">
+                        <span class="glyphicon glyphicon-earphone" aria-hidden="true"></span>&nbsp;
+                        <span class="phone-digits">8 800 350 84 70</span> 
+                    </div>
+                </div> 
+            </div>
+        </div>
+    </div>
+    <h1>Дистанционная бухгалтерия по-мужски!</h1>
+    <div class="first-man-wrapper">
+        <div>Первая</div>
+        <div>Мужская</div>
+        <div>Бухгалтерская</div>
+        <div>Компания</div>
+    </div>
+</div>
+
+<div class="container-fluid visible-xs-block visible-sm-block header-mob">
+    <div class="row">
+        <div class="col-md-12">
+            <a href="index.php"><div class="logo-wrapper-mob"></div></a>
+            <div class="right-side">
+                <button id="btn-free-consult-mob">Бесплатная консультация</button>
+            </div> 
+        </div>
+    </div>
+</div>
+
