fbaa0aaf679d0429c51b780212671939f94c901b
avenirs05 <avenirs05@gmail.com>
2017-06-22 17:50:53 +0300

    last

--- index.php
+++ index.php
@@ -17,12 +17,6 @@
     <script src="js/jquery-3.1.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="events-general.js"></script>
-    <script>
-        $(function () {
-            $('[data-toggle="tooltip"]').tooltip();
-        });
-    </script>
-    <!-- Yandex.Metrika counter --> <script type="text/javascript"> (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter45030163 = new Ya.Metrika({ id:45030163, clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = "https://mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks"); </script> <noscript><div><img src="https://mc.yandex.ru/watch/45030163" style="position:absolute; left:-9999px;" alt="" /></div></noscript> <!-- /Yandex.Metrika counter -->
 </head>
 <body>
 
@@ -163,9 +157,9 @@
                     <td class="text-center">Включает комплексное бухгалтерское сопровождение, пакет консультаций по правовым и налоговым вопросам</td>
                 </tr>
                 <tr>
-                    <td class="text-center cell-price">от 3 000 руб. в месяц</td>
-                    <td class="text-center cell-price">от 4 500 руб. в месяц</td>
-                    <td class="text-center cell-price">от 7 000 руб. в месяц</td>
+                    <td class="text-center cell-price">от 2 999 руб. в месяц</td>
+                    <td class="text-center cell-price">от 4 499 руб. в месяц</td>
+                    <td class="text-center cell-price">от 6 999 руб. в месяц</td>
                 </tr>
             </table>       
             <div id="look-all-price" class="text-center">
